102ce009e2124ed269e924af5ddf4fb2501b02b9
WJ <weijunn@hotmail.com>
2019-10-14 12:58:00 +0800

    v1

--- STEPL Website/js/index.js
+++ STEPL Website/js/index.js
@@ -23,14 +23,20 @@ function showDivs(n) {
 }
 
 
-
-function myFunction() {
+/** Hambyrger menu function */
+function mainNarbarFunction() {
   var x = document.getElementById("myTopnav");
+  var menu = document.getElementById("menu");
+  menu.classList.remove("fa-bars");
   if (x.className === "navbar__ul") {
     x.className += " responsive";
+    menu.classList.add("fa-times");
 
   } else {
     x.className = "navbar__ul";
+    menu.classList.remove("fa-times");
+    menu.classList.add("fa-bars");
+
   }
 }
 
